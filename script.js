6ab42feb6d17e9a7b547b40a5c585eaffc9eba17
dwightwpursuit <dwight.williams@pursuit.org>
2025-09-22 19:58:50 -0400

    Day 1: Set up project files and add scholarship data

---
+++ script.js
@@ -0,0 +1,84 @@
+// This array will hold all our scholarship data.
+const scholarships = [
+    // --- Computer Science Scholarships ---
+    {
+      name: "Amazon Future Engineer Scholarship",
+      major: "Computer Science",
+      link: "https://www.amazonfutureengineer.com/scholarships"
+    },
+    {
+      name: "Google Lime Scholarship",
+      major: "Computer Science",
+      link: "https://buildyourfuture.withgoogle.com/scholarships"
+    },
+    {
+      name: "Microsoft Scholarship Program",
+      major: "Computer Science", 
+      link: "https://www.microsoft.com/en-us/diversity/programs/students/scholarships"
+    },
+    {
+      name: "SAIC STEM Scholarship",
+      major: "Computer Science",
+      link: "https://www.saic.com/who-we-are/citizenship/stem"
+    },
+    {
+      name: "IEEE Computer Society Scholarship",
+      major: "Computer Science",
+      link: "https://www.computer.org/volunteering/awards/scholarships"
+    },
+    
+    // --- Nursing Scholarships ---
+    {
+      name: "HRSA Nurse Corps Scholarship Program",
+      major: "Nursing",
+      link: "https://bhw.hrsa.gov/programs/nurse-corps/scholarship"
+    },
+    {
+      name: "Johnson & Johnson Nursing Scholarships",
+      major: "Nursing",
+      link: "https://nursing.jnj.com/scholarships"
+    },
+    {
+      name: "AACN Nursing Scholarship Program",
+      major: "Nursing",
+      link: "https://www.aacnnursing.org/students/scholarships-financial-aid"
+    },
+    {
+      name: "Nurses Educational Funds Scholarships",
+      major: "Nursing",
+      link: "https://www.nefscholarships.org"
+    },
+    {
+      name: "Tylenol Future Care Scholarship",
+      major: "Nursing",
+      link: "https://www.tylenol.com/news/scholarship"
+    },
+    
+    // --- Business Scholarships ---
+    {
+      name: "NBMBAA Undergraduate Scholarship",
+      major: "Business",
+      link: "https://nbmbaa.org/scholarships"
+    },
+    {
+      name: "Coca-Cola Scholars Program",
+      major: "Business",
+      link: "https://www.coca-colascholarsfoundation.org"
+    },
+    {
+      name: "NSHSS Business & Entrepreneurship Scholarship",
+      major: "Business",
+      link: "https://www.nshss.org/scholarships"
+    },
+    {
+      name: "Ritchie-Jennings Memorial Scholarship",
+      major: "Business",
+      link: "https://www.acfe.com/scholarship-award"
+    },
+    {
+      name: "DECA Scholarships",
+      major: "Business",
+      link: "https://www.deca.org/scholarships"
+    }
+    // --- Stop adding scholarships above this line ---
+  ];
